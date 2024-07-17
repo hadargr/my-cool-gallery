b72e4929be5c90d25784185784d59134160af336
hadargr <37349836+hadargr@users.noreply.github.com>
2024-07-17 08:58:55 +0300

    Create page.tsx to lazy-suspense

---
+++ src/app/lazy-suspense/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { useState, useTransition } from "react";
+import Search from "../common/Search";
+import Gallery from "./Gallery";
+import { GalleryType } from "../common/types";
+
+export default function App() {
+    const [filter, setFilter] = useState('');
+    const [text, setText] = useState('');
+    const [_, startTransition] = useTransition();
+
+    const handleInputChange = (newFilter: string) => {
+        setText(newFilter)
+        startTransition(() => {
+            setFilter(newFilter)
+        })
+    }
+
+    return (
+        <div>
+            <Search onSearch={handleInputChange} text={text} />
+            {Array.from({ length: 11 }).map((_, i) => {
+                return i ? <Gallery filterText={filter} albumId={i} galleryType={GalleryType.Grid} /> : null
+            })}
+        </div>
+    );
+}
